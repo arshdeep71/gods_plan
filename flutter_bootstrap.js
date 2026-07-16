73654abe7746185f8443f61249c32eba8b981bd6
arshdeep71 <97976303+arshdeep71@users.noreply.github.com>
2026-07-16 05:18:27 +0000

    Deploying to gh-pages from @ arshdeep71/gods_plan@bfcbcc13e6f260563823c6159cfc6e8c6f13536f 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2526228059" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4189031998" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
