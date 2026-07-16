3236c6ee75c2689d84426a275b425ac29c082abb
arshdeep71 <darkwebhost01@gmail.com>
2026-07-16 19:46:51 +0530

    fix(web): cache database factory and flatten database path to prevent worker lock contention

--- lib/services/sqlite_factory_web.dart
+++ lib/services/sqlite_factory_web.dart
@@ -1,18 +1,20 @@
 import 'package:sqflite_common_ffi_web/sqflite_ffi_web.dart';
 import 'package:sqflite_common/sqlite_api.dart';
 
+DatabaseFactory? _cachedWebFactory;
+
 Future<Database> openSqliteDatabase(
   String path, {
   required int version,
   required Future<void> Function(Database, int) onCreate,
 }) async {
-  var databaseFactory = createDatabaseFactoryFfiWeb(
+  _cachedWebFactory ??= createDatabaseFactoryFfiWeb(
     options: SqfliteFfiWebOptions(
       sharedWorkerUri: Uri.parse('sqflite_sw.js'),
       forceAsBasicWorker: true,
     ),
   );
-  return await databaseFactory.openDatabase(
+  return await _cachedWebFactory!.openDatabase(
     path,
     options: OpenDatabaseOptions(
       version: version,
@@ -22,5 +24,5 @@ Future<Database> openSqliteDatabase(
 }
 
 Future<String> getSqliteDatabasesPath() async {
-  return 'gods_plan_web_db';
+  return '';
 }
